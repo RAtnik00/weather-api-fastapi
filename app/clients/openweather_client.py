92cd1bbae05b50c0699420e1a1fbc0c394d5b359
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-02-11 22:12:31 +0100

    Add OpenWeatherClient skeleton with basic interface

---
+++ app/clients/openweather_client.py
@@ -0,0 +1,13 @@
+class OpenWeatherService:
+    def __init__(self, base_url: str, api_key: str):
+        self.base_url = base_url
+        self.api_key = api_key
+
+    def get_current_weather(self, location: str) -> dict:
+        return {}
+
+    def get_forecast(self, location: str) -> dict:
+        return {}
+
+    def get_yesterday_weather(self, location: str) -> dict:
+        return {}
