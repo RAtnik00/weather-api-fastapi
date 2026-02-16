ff36af7178d49df0516fcdfccc3bfcb5e87715a6
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-02-16 16:12:04 +0100

    Define CookiesService interface for history and favorites in cookies

--- app/services/cookies_service.py
+++ app/services/cookies_service.py
@@ -1,22 +1,24 @@
 class CookiesService:
+    HISTORY_KEY = "weather_history"
+    FAVORITES_KEY = "weather_favorites"
 
-    def get_history(self):
+    def get_history(self, cookies: dict[str, str]) -> list[str]:
         pass
 
-    def add_to_history(self):
+    def add_to_history(self, history: list[str], location: str) -> list[str]:
         pass
 
-    def get_favorites(self):
+    def get_favorites(self, cookies: dict[str, str]) -> list[str]:
         pass
 
-    def add_favorite(self):
+    def add_favorite(self, favorites: list[str], location: str) -> list[str]:
         pass
 
-    def remove_favorite(self):
+    def remove_favorite(self, favorites: list[str], location: str) -> list[str]:
         pass
 
-    def encode_history(self):
+    def encode_history(self, history: list[str]) -> str:
         pass
 
-    def encode_favorites(self):
+    def encode_favorites(self, favorites: list[str]) -> str:
         pass
