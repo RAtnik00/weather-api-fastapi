5f0c66bfb8022375d5ea0eefde38134e30b3f457
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-02-16 15:58:31 +0100

    Add cookies state service interface for history and favorites

---
+++ app/services/cookies_service.py
@@ -0,0 +1,22 @@
+class CookiesService:
+
+    def get_history(self):
+        pass
+
+    def add_to_history(self):
+        pass
+
+    def get_favorites(self):
+        pass
+
+    def add_favorite(self):
+        pass
+
+    def remove_favorite(self):
+        pass
+
+    def encode_history(self):
+        pass
+
+    def encode_favorites(self):
+        pass
