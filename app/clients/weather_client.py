88e5c89e931fc5112d3d57cb25f23739cb8c295a
RAtnik00 <yaremenkodmitriy22@gmail.com>
2026-03-09 17:31:05 +0100

    Move OpenWeather response mapping from service to client models

--- app/clients/weather_client.py
+++ app/clients/weather_client.py
@@ -1,9 +1,10 @@
 from typing import Protocol
+from app.models.weather import CurrentWeatherData, ForecastData
 
 
 class WeatherClient(Protocol):
-    def get_current_weather(self, location: str) -> dict:
+    def get_current_weather(self, location: str) -> CurrentWeatherData:
         pass
 
-    def get_forecast(self, location: str) -> dict:
+    def get_forecast(self, location: str) -> ForecastData:
         pass
